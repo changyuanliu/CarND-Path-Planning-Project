dafac4a293f215ac85ad34a602aa9aedbdb65c7c
changyuanliu <changyuan@ustc.edu>
2021-03-06 10:59:12 -0500

    bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -57,8 +57,8 @@ int main() {
   // Have a reference velocity to target
   double ref_vel = 0.0; //unit: mph
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel]
+  h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+               &map_waypoints_dx,&map_waypoints_dy,&lane]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -123,7 +123,7 @@ int main() {
               double check_car_s = sensor_fusion[i][5];
 
               check_car_s += (double) (prev_size * 0.02 * check_speed);
-              if(((check_car_s > car_s) > 0) && ((check_car_s - car_s) < 30))
+              if(((check_car_s - car_s) > 0) && ((check_car_s - car_s) < 30))
               {
                 too_close = true;
               }
